78d1be0b40e23d91271da10c13e9f6e0bb876f05
Ayushi Dey <ayushidey@Ayushis-Laptop.local>
2024-03-28 14:52:38 +0530

    chore(#890): Edit script

--- android/src/main/java/io/mosip/tuvali/cryptography/WalletCryptoBox.java
+++ android/src/main/java/io/mosip/tuvali/cryptography/WalletCryptoBox.java
@@ -2,6 +2,5 @@ package io.mosip.tuvali.cryptography;
 
 public interface WalletCryptoBox {
     byte[] publicKey();
-    int testVar;
     SecretsTranslator buildSecretsTranslator(byte[] walletPublicKey);
 }
