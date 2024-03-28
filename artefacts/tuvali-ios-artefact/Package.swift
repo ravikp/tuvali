8e42195365c270f95e166336385ca8dc8b4228e0
Ayushi Dey <ayushidey@Ayushis-Laptop.local>
2024-03-28 11:42:59 +0530

    chore(#891): Edited Git Actions for Swift Version

--- artefacts/tuvali-ios-artefact/Package.swift
+++ artefacts/tuvali-ios-artefact/Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "test-folder",
+    name: "ios-tuvali-library",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "test-folder",
-            targets: ["test-folder"]),
+            name: "ios-tuvali-library",
+            targets: ["ios-tuvali-library"]),
     ],
     dependencies: [
         .package(url: "https://github.com/1024jp/GzipSwift", from: "6.0.0"),
@@ -19,9 +19,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "test-folder"),
+            name: "ios-tuvali-library"),
         .testTarget(
-            name: "test-folderTests",
-            dependencies: ["test-folder"]),
+            name: "ios-tuvali-libraryTests",
+            dependencies: ["ios-tuvali-library"]),
     ]
 )
