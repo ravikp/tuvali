e80c8e7b6779c5ce79d3337cfe9e80f0d0caa9d6
Abhishek Paul <abhishek.paul@thoughtworks.com>
2024-03-29 14:09:57 +0530

    feat(#891): changes in ios-demo-app

--- artefacts/swift-artefact/Package.swift
+++ artefacts/swift-artefact/Package.swift
@@ -1,27 +1,31 @@
-// swift-tools-version: 5.7.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.7.1
 import PackageDescription
 
 let package = Package(
     name: "ios-tuvali-library",
+    platforms: [
+       .macOS(.v11) // Specify macOS 11.0 as the minimum required version
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ios-tuvali-library",
             targets: ["ios-tuvali-library"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/1024jp/GzipSwift", from: Version(6, 0, 0)),
-	      .package(url: "https://github.com/ivanesik/CrcSwift.git", from: "0.0.3")
+        .package(url: "https://github.com/1024jp/GzipSwift", from: "6.0.0"),
+        .package(url: "https://github.com/ivanesik/CrcSwift.git", from: "0.0.3")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ios-tuvali-library"),
+            name: "ios-tuvali-library",
+            dependencies: [
+                .product(name: "Gzip", package: "GzipSwift"),
+                .product(name: "CrcSwift", package: "CrcSwift")
+            ]),
         .testTarget(
             name: "ios-tuvali-libraryTests",
             dependencies: ["ios-tuvali-library"]),
     ]
 )
+
+
